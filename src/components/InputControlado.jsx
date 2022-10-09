fa89571dcaf38517ad7111816205bd9315dfbaf6
FlorencioQuelca <florens.lonwe@gmail.com>
2022-10-08 23:42:15 -0400

    avances2

---
+++ src/components/InputControlado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputControlado = () => {
+  return (
+    <div>InputControlado</div>
+  )
+}
+
+export default InputControlado
---
+++ src/components/inputNoControlado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const inputNoControlado = () => {
+  return (
+    <div>inputNoControlado</div>
+  )
+}
+
+export default inputNoControlado
